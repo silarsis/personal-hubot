b66b3cf4c8e80f77aefddac265f7f8ca09e8277b
Kevin Littlejohn <kevin@littlejohn.id.au>
2014-07-19 07:52:19 +1000

    wercker CI

---
+++ Dockerfile
@@ -0,0 +1,26 @@
+FROM dockerfile/nodejs
+MAINTAINER Kevin Littlejohn <kevin@littlejohn.id.au>
+# Copied from desmondmorris/hubot
+
+# Install hubot & coffee-script globally
+RUN npm install -g hubot coffee-script
+
+# Create new hubot
+WORKDIR /usr/local/opt
+RUN hubot --create hubot
+WORKDIR /usr/local/opt/hubot
+RUN chmod +x bin/hubot
+RUN npm install
+RUN npm install forever -g
+RUN npm install hubot-slack hubot-flowdock --save
+ADD Procfile /usr/local/opt/hubot/Procfile
+
+# Create hubot system user
+RUN adduser --disabled-password --gecos "" hubot
+
+# Install redis-server and supervisor
+RUN apt-get -qqy install redis-server
+ADD start.sh /usr/local/bin
+
+EXPOSE 8080
+CMD /usr/local/bin/start.sh
---
+++ wercker_provision.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+#
+# Script to provision from wercker, building on orchard
+#
+# Needs/supports the following environment variables:
+# HUBOT_SLACK_TEAM
+# HUBOT_SLACK_TOKEN
+# HUBOT_SLACK_BOTNAME
+
+SRC="."
+ORCHARD_BIN="./orchard"
+ORCHARD_CMD="${ORCHARD_BIN}"
+DOCKER_CMD="${ORCHARD_CMD} docker"
+IMAGE_NAME="hubot"
+RUN_FLAGS="-d -t"
+VOLUME_FLAG=""
+BUILD=0
+RESTART=0
+
+while getopts ":br" opt; do
+  case $opt in
+    b)
+      BUILD=1
+      ;;
+    r)
+      RESTART=1
+      ;;
+  esac
+done
+
+set -e
+set -x
+
+[ -e ./orchard ] || {
+  curl -L -o ${ORCHARD_BIN} https://github.com/orchardup/go-orchard/releases/download/2.0.7/linux
+  chmod +x ${ORCHARD_BIN}
+  ${ORCHARD_CMD} hosts create
+}
+
+[ ${BUILD} -eq 1 ] && {
+  time ${DOCKER_CMD} build --rm -t ${IMAGE_NAME} ${SRC}
+}
+
+set +x
+
+[ ${RESTART} -eq 1 ] && {
+  ${DOCKER_CMD} kill ${IMAGE_NAME} || echo "could not kill ${IMAGE_NAME}"
+  ${DOCKER_CMD} rm ${IMAGE_NAME} || echo "could not remove ${IMAGE_NAME} container"
+  echo "${DOCKER_CMD} run ${RUN_FLAGS} <environment> --name ${IMAGE_NAME} ${IMAGE_NAME} /usr/local/bin/start.sh"
+  ${DOCKER_CMD} run ${RUN_FLAGS} ${VOLUME_FLAG} \
+    -e SLACK_API_URL=${SLACK_API_URL} \
+    -e LOGENTRIES_TOKEN=${LOGENTRIES_TOKEN} \
+    -e HUBOT_SLACK_TOKEN=${HUBOT_SLACK_TOKEN} \
+    -e HUBOT_SLACK_TEAM=${HUBOT_SLACK_TEAM} \
+    -e HUBOT_SLACK_BOTNAME=${HUBOT_SLACK_BOTNAME} \
+    --name ${IMAGE_NAME} \
+    ${IMAGE_NAME} /usr/local/bin/start.sh
+}
+exit 0
